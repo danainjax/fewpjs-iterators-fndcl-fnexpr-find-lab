cac622b40809e45a3bd544c73f2b8b76365601dc
danainjax <danainjax@gmail.com>
2021-06-07 10:44:51 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function superbowlWin(array) {
+     let win = array.find( array=> {
+         return array.result === "W"})
+    if (!!win == true) {
+        return win.year
+    } else 
+        return undefined
+
+    }
+    
+    
+         
+     
+
 
